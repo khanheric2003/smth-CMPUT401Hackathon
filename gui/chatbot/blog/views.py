959a3266981ec76145135801bcd3c2e22bfef4db
jfernan123 <jfernan1@ualberta.ca>
2024-01-20 13:49:08 -0700

    progress on bot

---
+++ gui/chatbot/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+
+def specific(request):
+    theList = [1,2,3,4,5]
+    return HttpResponse(theList)
+def article(request, article_id):
+    theID = article_id + 1
+    return render(request, 'blog/index.html', {"article_id": theID})
